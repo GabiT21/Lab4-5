f2c46c9d1fbf66338b002bf72c45c8cf608f94e4
GabiT21 <59895379+GabiT21@users.noreply.github.com>
2020-03-22 22:04:58 +0200

    Update Operations.cpp

--- 1Gabi/OOP/newLaborator4-5/Operations.cpp
+++ 1Gabi/OOP/newLaborator4-5/Operations.cpp
@@ -28,33 +28,3 @@ void stergereSub5(RepositoryArray& repo)
 }
 
 
-
-/*
-void repoSTLDeleteProjectsWithZeroBranchesOrCommits(RepositorySTL& projects)
-{
-	for (int i = 0; i < projects.dim(); i++)
-	{
-		Project crrProject = projects.getItemFromPos(i);
-		if (crrProject.getNoOfBranches() == 0 or crrProject.getTotalNoOfCommits() == 0)
-		{
-			projects.delElem(projects.getItemFromPos(i));
-			i--;
-		}
-	}
-}
-
-void repoArrayFindProjectsWithAtLeastKBranchesAndLCommits(RepositoryArray &projects, \
-	int k, int l, Project found[], int& m)
-{
-	m = 0;
-	for (int i = 0; i < projects.dim(); i++)
-	{
-		Project crrProject = projects.getItemFromPos(i);
-		if (crrProject.getNoOfBranches() >= k and crrProject.getTotalNoOfCommits() >= l)
-		{
-			found[m]=crrProject;
-			m++;
-		}
-	}
-}
-*/
